b958bd04f430d2bdd05d154226662eaa54587376
gomteang2dayo <as4290156@gmail.com>
2021-04-03 01:39:10 +0900

    반복문 없이 10진수 -> 2진수 문제 코드 #5

---
+++ JavaProgramming/BinaryConverter.java
@@ -0,0 +1,106 @@
+import java.util.Scanner;
+
+public class BinaryConverter {
+    public static void main(String[] args) {
+        Scanner stdin = new Scanner(System.in);
+        int num;
+        int a=0;
+        System.out.print("정수: ");
+        num= stdin.nextInt();
+        System.out.print(num+": ");
+        if(num/ 512>= 1) {
+            System.out.print(1);
+            num%= 512;
+            a = 1;
+        }
+        if(num/ 256>= 1) {
+            System.out.print(1);
+            num%= 256;
+            a = 1;
+        }
+        else{
+            if(a==1) {
+                System.out.print(0);
+            }
+        }
+
+        if(num/ 128>= 1) {
+            System.out.print(1);
+            num%= 128;
+            a = 1;
+        }
+        else{
+            if(a==1) {
+                System.out.print(0);
+            }
+        }
+
+        if(num/ 64>= 1) {
+            System.out.print(1);
+            num%= 64;
+            a = 1;
+        }
+        else{
+            if(a==1) {
+                System.out.print(0);
+            }
+        }
+        if(num/ 32>= 1) {
+            System.out.print(1);
+            num%= 32;
+            a = 1;
+        }
+        else{
+            if(a==1) {
+                System.out.print(0);
+            }
+        }
+        if(num/ 16>= 1) {
+            System.out.print(1);
+            num%= 16;
+            a = 1;
+        }
+        else{
+            if(a==1) {
+                System.out.print(0);
+            }
+        }
+        if(num/ 8>= 1) {
+            System.out.print(1);
+            num%= 8;
+            a = 1;
+        }
+        else{
+            if(a==1) {
+                System.out.print(0);
+            }
+        }
+        if(num/ 4>= 1) {
+            System.out.print(1);
+            num%= 4;
+            a = 1;
+        }
+        else{
+            if(a==1) {
+                System.out.print(0);
+            }
+        }
+        if(num/ 2>= 1) {
+            System.out.print(1);
+            num%= 2;
+            a = 1;
+        }
+        else{
+            if(a==1) {
+                System.out.print(0);
+            }
+        }
+        if(num>= 1) {
+            System.out.print(1);
+            num%= 1;
+        }
+        else{
+            System.out.print(0);
+        }
+    }
+}
