6fceb49a8e3f5e77317738b25c38c48287d77a1f
gomteang2dayo <as4290156@gmail.com>
2021-04-08 23:00:05 +0900

    Student 클래스를 객체를 생성하여 출력 문제 코드 #14

---
+++ JavaProgramming/Student.java
@@ -0,0 +1,10 @@
+public class Student {
+    public String name = "Kim";
+    public int rollno = 20180001;
+    public int age = 20;
+
+    public static void main(String[] args) {
+        Student S = new Student();
+        System.out.printf("학생의 이름: %s\n학생의 학번: %d\n학생의 나이:%d\n", S.name, S.rollno, S.age);
+    }
+}
